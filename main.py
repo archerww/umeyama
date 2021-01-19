a63c89fc3017a9cf9cd5e2fd85013f57ad60ff82
aochenwang <aochenwang@tencent.com>
2021-01-19 15:05:11 +0800

    a uyameya method demo with py

---
+++ main.py
@@ -0,0 +1,80 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# Press the green button in the gutter to run the script.
+import numpy as np
+UmeyamaResult = typing.Tuple[np.ndarray, np.ndarray, float]
+def umeyama_alignment(x: np.ndarray, y: np.ndarray,
+                      with_scale: bool = False) -> UmeyamaResult:
+    """
+    Computes the least squares solution parameters of an Sim(m) matrix
+    that minimizes the distance between a set of registered points.
+    Umeyama, Shinji: Least-squares estimation of transformation parameters
+                     between two point patterns. IEEE PAMI, 1991
+    :param x: mxn matrix of points, m = dimension, n = nr. of data points
+    :param y: mxn matrix of points, m = dimension, n = nr. of data points
+    :param with_scale: set to True to align also the scale (default: 1.0 scale)
+    :return: r, t, c - rotation matrix, translation vector and scale factor
+    """
+    if x.shape != y.shape:
+        raise GeometryException("data matrices must have the same shape")
+
+    # m = dimension, n = nr. of data points
+    m, n = x.shape
+
+    # means, eq. 34 and 35
+    mean_x = x.mean(axis=1)
+    mean_y = y.mean(axis=1)
+
+    # variance, eq. 36
+    # "transpose" for column subtraction
+    sigma_x = 1.0 / n * (np.linalg.norm(x - mean_x[:, np.newaxis])**2)
+
+    # covariance matrix, eq. 38
+    outer_sum = np.zeros((m, m))
+    for i in range(n):
+        outer_sum += np.outer((y[:, i] - mean_y), (x[:, i] - mean_x))
+    cov_xy = np.multiply(1.0 / n, outer_sum)
+
+    # SVD (text betw. eq. 38 and 39)
+    u, d, v = np.linalg.svd(cov_xy)
+    if np.count_nonzero(d > np.finfo(d.dtype).eps) < m - 1:
+        raise GeometryException("Degenerate covariance rank, "
+                                "Umeyama alignment is not possible")
+
+    # S matrix, eq. 43
+    s = np.eye(m)
+    if np.linalg.det(u) * np.linalg.det(v) < 0.0:
+        # Ensure a RHS coordinate system (Kabsch algorithm).
+        s[m - 1, m - 1] = -1
+
+    # rotation, eq. 40
+    r = u.dot(s).dot(v)
+
+    # scale & translation, eq. 42 and 41
+    c = 1 / sigma_x * np.trace(np.diag(d).dot(s)) if with_scale else 1.0
+    t = mean_y - np.multiply(c, r.dot(mean_x))
+
+    return r, t, c
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print('Hi,'+name)  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    matrix = numpy.matrix([[1,2,3],
+              [4,5,6],
+              [7,8,9]])
+    me = matrix.mean(1)
+    print matrix
+    print me
+    print matrix.shape
+    a = '   jjajaa    1'
+    print(a.strip())
+    print(a.lstrip())  #
+    print(a.rstrip())  #
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
